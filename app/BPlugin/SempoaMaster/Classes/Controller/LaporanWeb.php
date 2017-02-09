5aa9cbd13a357dc8908085cbc0511a19b74fe1a0
pipoo <efindi.ongso@gmail.com>
2017-02-09 12:53:05 +0700

    coba iuran bulanan  daan

--- app/BPlugin/SempoaMaster/Classes/Controller/LaporanWeb.php
+++ app/BPlugin/SempoaMaster/Classes/Controller/LaporanWeb.php
@@ -89,16 +89,13 @@ class LaporanWeb extends WebService
     //TC
     public function create_operasional_pembayaran_iuran_bulanan_tc()
     {
-        echo "Bulanan";
-
-
-//        die();
         $myorg = AccessRight::getMyOrgID();
         echo $myorg;
         $bln = isset($_GET['bln']) ? addslashes($_GET['bln']) : date("n");
         $thn = isset($_GET['thn']) ? addslashes($_GET['thn']) : date("Y");
         $tc_id = isset($_GET['tc_id']) ? addslashes($_GET['tc_id']) : AccessRight::getMyOrgID();
         $murid = new MuridModel();
+        // Status cuti dikeluarkan
         $arrMurid = $murid->getWhere("(status = 1) AND murid_tc_id = '$tc_id' ORDER BY nama_siswa ASC");
         $status = new MuridWeb2Model();
         $arrs = $status->getAll();
