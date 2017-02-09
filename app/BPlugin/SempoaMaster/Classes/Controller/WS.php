1a591a01067d326b5d4839c07153a575fc2b0544
pipoo <efindi.ongso@gmail.com>
2017-02-09 12:21:16 +0700

    coba iuran bulanan  daan

--- app/BPlugin/SempoaMaster/Classes/Controller/WS.php
+++ app/BPlugin/SempoaMaster/Classes/Controller/WS.php
@@ -638,7 +638,7 @@ FROM {$tc->table_name} HAVING distance < 25 ORDER by distance";
 
                         $iuranBulanan = new IuranBulanan();
                         $iuranBulanan->getWhereOne("bln_murid_id = $mk->id_murid AND bln_mon = $bln AND bln_tahun = $thn AND bln_tc_id=$myorg");
-                        pr($iuranBulanan);
+//                        pr($iuranBulanan);
                         ?>
 
                         <tr id='payment_<?= $iuranBulanan->bln_id; ?>' class="<?if ($iuranBulanan->bln_status) {?>sudahbayar <?}else{?> belumbayar<?}?>">
@@ -682,7 +682,7 @@ FROM {$tc->table_name} HAVING distance < 25 ORDER by distance";
                             })
                         </script>
                         <?
-                        die();
+//                        die();
                     }
                     ?>
                     </tbody>
